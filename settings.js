ba4db4c533fefdd9d03e5c4dd5007e72b0afa0e3
mrelephant122 <09samaz09@gmail.com>
2026-07-08 11:12:26 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝕀ℂ𝕆ℕ-𝕏 𝕄𝔻',
+  author: 'Mr Elephant',
+  botName: "𝕀ℂ𝕆ℕ-𝕏 𝕄𝔻",
+  botOwner: 'Mr Elephant', // Your name
+  ownerNumber: '263782313021', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+  updateZipUrl: "https://github.com/",
+};
+
+module.exports = settings;
